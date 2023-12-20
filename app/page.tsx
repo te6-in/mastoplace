337edbbab111dc46e95c9a7e069225ab0ea14560
Chanhwi Joo <56245920+te6-in@users.noreply.github.com>
2023-12-21 01:40:13 +0900

    add another verified account

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import { Redirect } from "@/components/Redirect";
 export default function Base() {
 	return (
 		<>
-			<a rel="me" href="https://mastodon.social/@mastoplace"></a>
-			<a rel="me" href="https://fosstodon.org/@mastoplace"></a>
+			<div className="hidden">
+				<a rel="me" href="https://mastodon.social/@mastoplace"></a>
+				<a rel="me" href="https://fosstodon.org/@mastoplace"></a>
+				<a rel="me" href="https://mas.to/@he"></a>
+			</div>
 			<Redirect />
 		</>
 	);
